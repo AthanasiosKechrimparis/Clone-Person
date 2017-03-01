895e5d130309104e0649073be0d499ac5186726a
Athanasios Kechrimparis <a.kechrimparis@outlook.com.gr>
2017-03-01 21:51:46 +0100

    Add new

---
+++ CloneExcercise/CloneExcercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
